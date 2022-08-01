8714ede8eecce697f3a29b43c47e6bda70430903
Raphael Machado <raphael.mm.91@gmail.com>
2022-08-01 14:06:56 -0300

    photos update

--- src/util/players.js
+++ src/util/players.js
@@ -1,68 +1,107 @@
+import gabigol from "../assets/players/gabigol.jpg"
+import arrascaeta from "../assets/players/arrascaeta.jpg"
+import ayrtonlucas from "../assets/players/ayrtonlucas.jpg"
+import brunohenrique from "../assets/players/brunohenrique.jpg"
+import cebolinha from "../assets/players/cebolinha.jpg"
+import cleiton from "../assets/players/cleiton.jpg"
+import davidluiz from "../assets/players/davidluiz.jpg"
+import diegoribas from "../assets/players/diegoribas.jpg"
+import evertonribeiro from "../assets/players/evertonribeiro.jpg"
+import fabriciobruno from "../assets/players/fabriciobruno.jpg"
+import filipeluis from "../assets/players/filipeluis.jpg"
+import joaogomes from "../assets/players/joaogomes.jpg"
+import lazaro from "../assets/players/lazaro.jpg"
+import leopereira from "../assets/players/leopereira.jpg"
+import marinho from "../assets/players/marinho.jpg"
+import matheuscunha from "../assets/players/matheuscunha.jpg"
+import matheusfranca from "../assets/players/matheusfranca.jpg"
+import matheuzinho from "../assets/players/matheuzinho.jpg"
+import neneca from "../assets/players/neneca.jpg"
+import pablo from "../assets/players/pablo.jpg"
+import pedro from "../assets/players/pedro.jpg"
+import rodinei from "../assets/players/rodinei.jpg"
+import rodrigocaio from "../assets/players/rodrigocaio.jpg"
+import santos from "../assets/players/santos.jpg"
+import thiagomaia from "../assets/players/thiagomaia.jpg"
+import vidal from "../assets/players/vidal.jpg"
+import vitinho from "../assets/players/vitinho.jpg"
+import diegoalves from "../assets/players/diegoalves.jpg"
 const players = [
   {
     name: "Gabigol",
     num: 9,
-    img: "https://img.a.transfermarkt.technology/portrait/big/244275-1623847700.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/244275-1623847700.jpg?lm=1",
+    img: gabigol,
     pos: "A"
   },
   {
     name: "Arrascaeta",
     num: 14,
-    img: "https://img.a.transfermarkt.technology/portrait/big/248410-1610006665.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/248410-1610006665.jpg?lm=1",
+    img: arrascaeta,
     pos: "M"
   },
   {
     name: "Pedro",
     num: 21,
-    img: "https://img.a.transfermarkt.technology/portrait/big/432895-1533634320.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/432895-1533634320.jpg?lm=1",
+    img: pedro,
     pos: "A"
   },
   {
     name: "Éverton Ribeiro",
     num: 7,
-    img: "https://img.a.transfermarkt.technology/portrait/big/52896-1610006872.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/52896-1610006872.jpg?lm=1",
+    img: evertonribeiro,
     pos: "M"
   },
   {
     name: "João Gomes",
     num: 35,
-    img: "https://img.a.transfermarkt.technology/portrait/big/735570-1623845811.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/735570-1623845811.jpg?lm=1",
+    img: joaogomes,
     pos: "M"
   },
   {
     name: "Vidal",
     num: 32,
-    img: "https://img.a.transfermarkt.technology/portrait/big/37666-1502276644.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/37666-1502276644.jpg?lm=1",
+    img: vidal,
     pos: "M"
   },
   {
     name: "Filipe Luis",
     num: 16,
-    img: "https://img.a.transfermarkt.technology/portrait/big/21725-1447152742.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/21725-1447152742.jpg?lm=1",
+    img: filipeluis,
     pos: "L"
   },
   {
     name: "David Luiz",
     num: 23,
-    img: "https://img.a.transfermarkt.technology/portrait/big/46741-1472656986.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/46741-1472656986.jpg?lm=1",
+    img: davidluiz,
     pos: "D"
   },
   {
     name: "Rodrigo Caio",
     num: 3,
-    img: "https://img.a.transfermarkt.technology/portrait/big/172054-1471615155.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/172054-1471615155.jpg?lm=1",
+    img: rodrigocaio,
     pos: "D"
   },
   {
     name: "Rodinei",
     num: 22,
-    img: "https://img.a.transfermarkt.technology/portrait/big/185966-1611222363.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/185966-1611222363.jpg?lm=1",
+    img: rodinei,
     pos: "L"
   },
   {
     name: "Santos",
     num: 20,
-    img: "https://img.a.transfermarkt.technology/portrait/big/140811-1554731294.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/140811-1554731294.jpg?lm=1",
+    img: santos,
     pos: "G"
   },
   {
@@ -74,91 +113,106 @@ const players = [
   {
     name: "Cebolinha",
     num: 19,
-    img: "https://img.a.transfermarkt.technology/portrait/big/321239-1603795820.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/321239-1603795820.jpg?lm=1",
+    img: cebolinha,
     pos: "A"
   },
   {
     name: "Thiago Maia",
     num: 8,
-    img: "https://img.a.transfermarkt.technology/portrait/big/294873-1457513224.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/294873-1457513224.jpg?lm=1",
+    img: thiagomaia,
     pos: "M"
   },
   {
     name: "Lázaro",
     num: 13,
-    img: "https://img.a.transfermarkt.technology/portrait/big/574772-1574058091.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/574772-1574058091.jpg?lm=1",
+    img: lazaro,
     pos: "M"
   },
   {
     name: "Vitinho",
     num: 11,
-    img: "https://img.a.transfermarkt.technology/portrait/big/213605-1447749971.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/213605-1447749971.jpg?lm=1",
+    img: vitinho,
     pos: "A",
   },
   {
     name: "Bruno Henrique",
     num: 27,
-    img: "https://img.a.transfermarkt.technology/portrait/big/353108-1605189960.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/353108-1605189960.jpg?lm=1",
+    img: brunohenrique,
     pos: "A"
   },
   {
     name: "Léo Pereira",
     num: 4,
-    img: "https://img.a.transfermarkt.technology/portrait/big/288431-1494244050.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/288431-1494244050.jpg?lm=1",
+    img: leopereira,
     pos: "D"
   },
   {
     name: "Matheuzinho",
     num: 34,
-    img: "https://img.a.transfermarkt.technology/portrait/big/594226-1639506313.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/594226-1639506313.jpg?lm=1",
+    img: matheuzinho,
     pos: "L"
   },
   {
     name: "Fabricio Bruno",
     num:15,
-    img: "https://img.a.transfermarkt.technology/portrait/big/432773-1652345536.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/432773-1652345536.jpg?lm=1",
+    img:fabriciobruno,
     pos: "D"
   },
   {
     name: "Pablo",
     num: 30,
-    img: "https://img.a.transfermarkt.technology/portrait/big/149577-1617369576.png?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/149577-1617369576.png?lm=1",
+    img: pablo,
     pos: "D"
   },
   {
     name: "Ayrton Lucas",
     num: 6,
-    img: "https://img.a.transfermarkt.technology/portrait/big/349585-1632577645.png?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/349585-1632577645.png?lm=1",
+    img: ayrtonlucas,
     pos: "L"
   },
   {
     name: "Diego Alves",
     num: 1,
-    img: "https://img.a.transfermarkt.technology/portrait/big/42373-1543845950.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/42373-1543845950.jpg?lm=1",
+    img: diegoalves,
     pos: "G"
   },
   {
     name: "Hugo Neneca",
     num: 45,
-    img: "https://img.a.transfermarkt.technology/portrait/big/574901-1541567933.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/574901-1541567933.jpg?lm=1",
+    img: neneca,
     pos: "G"
   },
   {
     name: "Marinho",
     num: 31,
-    img: "https://img.a.transfermarkt.technology/portrait/big/74758-1558620761.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/74758-1558620761.jpg?lm=1",
+    img: marinho,
     pos: "A"
   },
   {
     name: "Diego Ribas",
     num: 10,
-    img: "https://img.a.transfermarkt.technology/portrait/big/4248-1506074640.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/4248-1506074640.jpg?lm=1",
+    img: diegoribas,
     pos: "M"
   },
   {
     name: "Matheus França",
     num: 42,
-    img: "https://img.a.transfermarkt.technology/portrait/big/743597-1626626932.jpg?lm=1",
+    // img: "https://img.a.transfermarkt.technology/portrait/big/743597-1626626932.jpg?lm=1",
+    img: matheusfranca,
     pos: "M",
   },
   {
