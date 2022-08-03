ee7028836eef50a1dc66d75fe483d8ffe42fc7b3
Raphael Machado <raphael.mm.91@gmail.com>
2022-08-03 17:21:14 -0300

    final touches

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
+import { VscFoldDown } from "react-icons/vsc";
+
 function Header() {
+  const [mobileScreen, setMobileScreen] = useState(false);
+  useEffect(() => {
+    window.addEventListener("resize", checkScreen);
+    if (mobileScreen) console.log("porra!");
+  }, [mobileScreen]);
+
+  const checkScreen = () =>
+    window.innerWidth < 650 ? setMobileScreen(true) : setMobileScreen(false);
+
   return (
     <div className="header">
-      <h1 id="title">
-        <span className="red-text">FLA</span>MAKER
-      </h1>
-      <p className="description">escale seu time favorito do Flamengo</p>
+      <div>
+        <h1 id="title">
+          <span className="red-text">FLA</span>MAKER
+        </h1>
+        <p className="description">escale seu time favorito do Flamengo</p>{" "}
+      </div>
+      {mobileScreen ? (
+        <div className="arrow-down">
+          <VscFoldDown />
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
-export {Header}
+export { Header };
